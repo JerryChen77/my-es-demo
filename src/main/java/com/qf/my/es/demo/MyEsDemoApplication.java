4627c25d28672297008921faba9d5a7d9ee9c29b
Jerry7901 <790165133@qq.com>
2021-09-26 21:20:40 +0800

    t

---
+++ src/main/java/com/qf/my/es/demo/MyEsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qf.my.es.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qf/my/es/demo/MyEsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.my.es.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
